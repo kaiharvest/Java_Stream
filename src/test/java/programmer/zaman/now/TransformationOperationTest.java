e49ac43c17bb690c0de0b1ac814556c77c09e3e7
kaiharvest <indradwiprabowo27@gmail.com>
2025-10-02 09:44:26 +0700

    Intermediate and Terminal Operation dan flat map

--- src/test/java/programmer/zaman/now/TransformationOperationTest.java
+++ src/test/java/programmer/zaman/now/TransformationOperationTest.java
@@ -3,6 +3,7 @@ package programmer.zaman.now;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class TransformationOperationTest {
 
@@ -11,6 +12,15 @@ public class TransformationOperationTest {
         List.of("Indra", "Dwi", "Prabowo").stream()
                 .map(name -> name.toUpperCase())
                 .map(upper -> upper.length())
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void testFlatMapOperation() {
+        List.of("Indra", "Dwi", "Prabowo").stream()
+                .map(name -> name.toUpperCase())
+                .flatMap(upper -> Stream.of(upper, upper.length()))
+                .flatMap(value -> Stream.of(value, value, value))
                 .forEach(System.out::println);
     }
 
